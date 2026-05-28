f2ff6a6c5e9d400cfb60d1ee14812bc59d79f755
gaston <gastschz323@gmail.com>
2026-05-28 16:03:35 -0300

    Se terminaron las actividades practicas

--- TP.js
+++ TP.js
@@ -34,3 +34,47 @@
     //  num = num + i;
 // }
 // console.log(`La suma total es: ${num}`);
+
+// Punto 6
+// let i=0;
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(`El número ${i} es par`);
+//     } else {
+//        console.log(`El numero es impar ${i}`);
+//     }
+// }
+
+// Punto 7
+// const estudiante={
+//     nomb:"Dante",
+//     apellido:"Perez",
+//     nota:5,
+// }
+// let fin=(estudiante.nota>=6)? "Aprobado": "Desaprobado";
+// console.log(`Estudiante ${estudiante.nomb} ${estudiante.apellido}. Nota:${estudiante.nota} ${fin}`);
+
+// Punto 8
+// const notas = [5, 6, 9, 2, 10];
+// let suma = 0;
+// for (let i=0; i< notas.length; i++){
+//     suma= suma+i
+// }
+// let prom=suma/notas.length;
+// let notpromed=(prom>=6)? "Aprobado": "Desaprobado";
+// console.log(`Las notas son: ${notas} Promedio: ${prom} ${notpromed}`)
+
+// Punto 9
+// const nombre="Gaston";
+// const notas=[8, 5, 7 ,8 ,7, 1];
+// let suma=0;
+// for (let i=0; i< notas.length; i++){
+//     suma= suma+notas[i];
+// }
+// let prom =(suma/notas.length)
+// if (prom >= 7)
+//     console.log(`${prom.toFixed(1)} Muy bueno`)
+// else if (prom >=6)
+//     console.log(`${prom.toFixed(1)} Aprobado`)
+// else
+//     console.log(`${prom.toFixed(1)} Desaprobado`)
