6a57bfac48259e26f2ca11b4253c9b0fdc7a491b
gaston <gastschz323@gmail.com>
2026-05-28 12:12:42 -0300

    Punto 1-5

---
+++ TP.js
@@ -0,0 +1,36 @@
+// Punto 1
+// const nombre="Gaston";
+// let edad= 18;
+// let carrera= "Desarrollo de Software Multiplataforma";
+// let cursa= true;
+// console.log(`Mi nombre es ${nombre}. Tengo ${edad} años, estudio ${carrera} y mi estado de cursado actual es: ${cursa}`)
+
+// Punto 2
+// const nota= 10
+// if (nota>=6) 
+//     console.log("Aprobado")
+// else
+//     console.log("Desaprobado")
+
+// Punto 3
+// let edad= 13
+// if (edad < 13)
+//     console.log("Niño")
+// else if(edad <=17)
+//     console.log("Adolescente")
+// else
+//     console.log("Adulto")
+
+// Punto 4
+// let num=1
+// for (let i=1; i <= 10; i++) {
+//     let resultado = num * i;
+// console.log(`${num} x ${i} = ${resultado}`);
+// }
+
+// Punto 5
+// let num=0;
+// for (let i=1; i <=100; i++) {
+    //  num = num + i;
+// }
+// console.log(`La suma total es: ${num}`);
